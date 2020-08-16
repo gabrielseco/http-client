649705ec0dbf76d4fd97de50404fc9e1cdf58772
Gabriel García <ggarciaseco@gmail.com>
2020-08-17 00:48:49 +0200

    Add types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+import type {
+  AxiosResponse as HttpClientResponse,
+  AxiosError as HttpClientError
+} from 'axios';
+
 import { HttpClient } from './HttpClient';
 
 export { HttpClient };
+
+export type { HttpClientResponse, HttpClientError };
