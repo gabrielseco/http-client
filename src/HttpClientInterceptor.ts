2a5ea0bf26e925c39ff31aa87b45395044b276df
Gabriel García <ggarciaseco@gmail.com>
2020-08-18 18:01:09 +0200

    fix interceptor

--- src/HttpClientInterceptor.ts
+++ src/HttpClientInterceptor.ts
@@ -1,10 +1,9 @@
 import axios, { AxiosResponse, AxiosError } from 'axios';
 
 export const HttpClientInterceptor = (): Promise<AxiosResponse> =>
-  new Promise((resolve, reject) => {
+  new Promise((_, reject) => {
     axios.interceptors.response.use(
       (response: AxiosResponse) => {
-        resolve(response);
         return response;
       },
       (error: AxiosError) => {
