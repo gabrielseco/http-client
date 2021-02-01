945afab8e11bfb18b86e1029f791c0ab37c61666
Gabriel García <ggarciaseco@gmail.com>
2021-02-01 07:33:35 +0100

    HttpClient responseType

--- src/HttpClient.ts
+++ src/HttpClient.ts
@@ -1,6 +1,10 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { ResponseType, AxiosResponse } from 'axios';
 
 type StringObject = { [key: string]: string };
+
+type Options = {
+  responseType?: ResponseType;
+};
 
 export class HttpClient {
   private baseUrl: string;
@@ -19,59 +23,74 @@ export class HttpClient {
 
   get<ApiResponse>({
     pathname,
-    params
+    params,
+    options
   }: {
     pathname: string;
     params?: Record<string, unknown>;
+    options?: Options;
   }): Promise<AxiosResponse<ApiResponse>> {
     return axios.get(`${this.baseUrl}${pathname}`, {
       params,
+      responseType: options?.responseType || 'json',
       headers: this.headers
     });
   }
 
   post<Payload, ApiResponse>({
     pathname,
-    payload
+    payload,
+    options
   }: {
     pathname: string;
     payload: Payload;
+    options?: Options;
   }): Promise<AxiosResponse<ApiResponse>> {
     return axios.post(`${this.baseUrl}${pathname}`, payload, {
+      responseType: options?.responseType || 'json',
       headers: this.headers
     });
   }
 
   put<Payload, ApiResponse>({
     pathname,
-    payload
+    payload,
+    options
   }: {
     pathname: string;
     payload: Payload;
+    options?: Options;
   }): Promise<AxiosResponse<ApiResponse>> {
     return axios.put(`${this.baseUrl}${pathname}`, payload, {
+      responseType: options?.responseType || 'json',
       headers: this.headers
     });
   }
 
   patch<Payload, ApiResponse>({
     pathname,
-    payload
+    payload,
+    options
   }: {
     payload: Payload;
     pathname: string;
+    options?: Options;
   }): Promise<AxiosResponse<ApiResponse>> {
     return axios.patch(`${this.baseUrl}${pathname}`, payload, {
+      responseType: options?.responseType || 'json',
       headers: this.headers
     });
   }
 
   delete<ApiResponse>({
-    pathname
+    pathname,
+    options
   }: {
     pathname: string;
+    options?: Options;
   }): Promise<AxiosResponse<ApiResponse>> {
     return axios.delete(`${this.baseUrl}${pathname}`, {
+      responseType: options?.responseType || 'json',
       headers: this.headers
     });
   }
